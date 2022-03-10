96bd2b70bef98e6df7d369b2b56dfaefd20d5520
mar-ekm <>
2022-03-10 10:43:18 +0000

    Initial Commit

--- developer/scripts/bhol.sh
+++ developer/scripts/bhol.sh
@@ -8,52 +8,52 @@ function replace_json_field {
 }
 
 # Check if SUFFIX envvar exists
-if [[ -z "558288" ]]; then
+if [[ -z "${MCW_SUFFIX}" ]]; then
     echo "Please set the MCW_SUFFIX environment variable to a unique three character string."
     exit 1
 fi
 
-if [[ -z "mar-ekm" ]]; then
+if [[ -z "${MCW_GITHUB_USERNAME}" ]]; then
     echo "Please set the MCW_GITHUB_USERNAME environment variable to your Github Username"
     exit 1
 fi
 
-if [[ -z "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" ]]; then
+if [[ -z "${MCW_GITHUB_TOKEN}" ]]; then
     echo "Please set the MCW_GITHUB_TOKEN environment variable to your Github Token"
     exit 1
 fi
 
 if [[ -z "${MCW_GITHUB_URL}" ]]; then
-    MCW_GITHUB_URL=https://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@github.com/mar-ekm/Fabmedical.git
+    MCW_GITHUB_URL=https://$MCW_GITHUB_TOKEN@github.com/$MCW_GITHUB_USERNAME/Fabmedical.git
 fi
 
-git config --global user.email "contact@martinekman.com"
-git config --global user.name "mar-ekm"
+git config --global user.email "$MCW_GITHUB_EMAIL"
+git config --global user.name "$MCW_GITHUB_USERNAME"
 
 cp -R ~/MCW-Cloud-native-applications/Hands-on\ lab/lab-files/developer ~/Fabmedical
 cd ~/Fabmedical
 git init
-git remote add origin https://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@github.com/mar-ekm/Fabmedical.git
+git remote add origin $MCW_GITHUB_URL
 
 git config --global --unset credential.helper
 git config --global credential.helper store
 
 # Configuring github workflows
 cd ~/Fabmedical
-sed -i "s/\[SUFFIX\]/558288/g" ~/Fabmedical/.github/workflows/content-init.yml
-sed -i "s/\[SUFFIX\]/558288/g" ~/Fabmedical/.github/workflows/content-api.yml
-sed -i "s/\[SUFFIX\]/558288/g" ~/Fabmedical/.github/workflows/content-web.yml
+sed -i "s/\[SUFFIX\]/$MCW_SUFFIX/g" ~/Fabmedical/.github/workflows/content-init.yml
+sed -i "s/\[SUFFIX\]/$MCW_SUFFIX/g" ~/Fabmedical/.github/workflows/content-api.yml
+sed -i "s/\[SUFFIX\]/$MCW_SUFFIX/g" ~/Fabmedical/.github/workflows/content-web.yml
 
 # Commit changes
 git add .
 git commit -m "Initial Commit"
 
 # Get ACR credentials and add them as secrets to Github
-ACR_CREDENTIALS=$(az acr credential show -n fabmedical558288)
+ACR_CREDENTIALS=$(az acr credential show -n fabmedical$MCW_SUFFIX)
 ACR_USERNAME=$(jq -r -n '$input.username' --argjson input "$ACR_CREDENTIALS")
 ACR_PASSWORD=$(jq -r -n '$input.passwords[0].value' --argjson input "$ACR_CREDENTIALS")
 
-GITHUB_TOKEN=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
+GITHUB_TOKEN=$MCW_GITHUB_TOKEN
 cd ~/Fabmedical
 echo $GITHUB_TOKEN | gh auth login --with-token
 gh secret set ACR_USERNAME -b "$ACR_USERNAME"
